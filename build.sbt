a9b103a08c92c0a7b05ee6c648c0ff5ba7256079
Gal Bar Mashiah <galbarm@gmail.com>
2018-06-03 21:29:03 +0300

    update google spreadsheet

--- build.sbt
+++ build.sbt
@@ -5,3 +5,6 @@ version := "0.1"
 scalaVersion := "2.12.6"
 
 libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.5"
+libraryDependencies += "com.google.api-client" % "google-api-client" % "1.23.0"
+libraryDependencies += "com.google.oauth-client" % "google-oauth-client-jetty" % "1.23.0"
+libraryDependencies += "com.google.apis" % "google-api-services-sheets" % "v4-rev525-1.23.0"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,38 @@
-import java.io.File
+import java.io._
 import java.time.YearMonth
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 import com.github.tototoshi.csv._
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver
+import com.google.api.client.googleapis.auth.oauth2.{GoogleAuthorizationCodeFlow, GoogleClientSecrets}
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.store.FileDataStoreFactory
+import com.google.api.services.sheets.v4.Sheets
+import com.google.api.services.sheets.v4.model.ValueRange
 
 import scala.collection.mutable
+import java.util
+import java.util.Collections
+
+import com.google.api.services.sheets.v4.SheetsScopes
+
+import scala.io.Source
 
 object Main {
 
+  private val JSON_FACTORY = JacksonFactory.getDefaultInstance
+
   def main(args: Array[String]): Unit = {
     println(s"file: ${args(0)}")
-
     val months = parse(args(0))
-
-    val what = 6
+    updateSheets(months)
   }
 
-  def parse(file: String): List[(YearMonth, Balance)] = {
+  private def parse(file: String): List[(YearMonth, Balance)] = {
     val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("MMM ''uu", Locale.ENGLISH)
     val months = mutable.HashMap[YearMonth, Balance]()
     val reader = CSVReader.open(new File(file))
@@ -41,5 +56,37 @@ object Main {
     })
 
     months.toList.sortBy(_._1)
+  }
+
+
+  private def updateSheets(months: List[(YearMonth, Balance)]): Unit = {
+    val HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport
+    val spreadsheetId = Source.fromResource("spreadsheet_id.txt").mkString
+    val service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+      .setApplicationName("YnabNetWorthFormatter")
+      .build()
+
+
+    val values: util.List[util.List[AnyRef]] = new util.ArrayList()
+
+    values.add(util.Arrays.asList("Date", "Assets", "Debts", "Net Worth"))
+    months.foreach(month =>
+      values.add(util.Arrays.asList(month._1.toString, month._2.assets.toString, month._2.debts.toString, month._2.netWorth.toString)))
+
+    service.spreadsheets().values().update(spreadsheetId, Source.fromResource("sheet_name.txt").mkString, new ValueRange().setValues(values)).setValueInputOption("USER_ENTERED").execute()
+  }
+
+
+  private def getCredentials(HTTP_TRANSPORT: NetHttpTransport) = {
+    val clientSecrets = GoogleClientSecrets
+      .load(JSON_FACTORY, Source.fromResource("client_id.json").reader())
+
+    val flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, Collections.singletonList(SheetsScopes.SPREADSHEETS))
+      .setDataStoreFactory(new FileDataStoreFactory(new File("credentials")))
+      .setAccessType("offline")
+      .build
+
+    new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver)
+      .authorize("user")
   }
 }
